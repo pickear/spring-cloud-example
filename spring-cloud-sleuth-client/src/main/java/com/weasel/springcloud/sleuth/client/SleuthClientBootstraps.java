fcd38ff8f0be0d6a80530339d95f98f8259e6abb
pickear <pickear@gmail.com>
2017-06-26 10:13:03 +0800

    init

---
+++ spring-cloud-sleuth-client/src/main/java/com/weasel/springcloud/sleuth/client/SleuthClientBootstraps.java
@@ -0,0 +1,12 @@
+package com.weasel.springcloud.sleuth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthClientBootstraps {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthClientBootstraps.class, args);
+	}
+}
