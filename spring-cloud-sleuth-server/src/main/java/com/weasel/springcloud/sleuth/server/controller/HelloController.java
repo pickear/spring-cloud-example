dbb70fb9b08cc6f6b170915810a4b289b4398eac
pickear <pickear@gmail.com>
2017-06-28 15:10:31 +0800

    解决在线程池中无法使用trace和span

--- spring-cloud-sleuth-server/src/main/java/com/weasel/springcloud/sleuth/server/controller/HelloController.java
+++ spring-cloud-sleuth-server/src/main/java/com/weasel/springcloud/sleuth/server/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.weasel.springcloud.sleuth.server.controller;
 
-import com.weasel.springcloud.sleuth.server.helper.ExecutorServiceHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.SpanNamer;
+import org.springframework.cloud.sleuth.TraceRunnable;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,22 +25,28 @@ public class HelloController {
 
     private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
     private final static ExecutorService executorService = Executors.newCachedThreadPool();
-    private final static ExecutorService wrapExecutorService = ExecutorServiceHelper.wrapExecutorService(Executors.newCachedThreadPool());
+    @Autowired
+    private Tracer tracer;
+    @Autowired
+    private SpanNamer spanNamer;
+    @Autowired
+    private BeanFactory beanFactory;
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         logger.info("[server] reponse client hello...");
-        executorService.submit(new Runnable() {
+        executorService.submit(new TraceRunnable(tracer,spanNamer,new Runnable(){
             @Override
             public void run() {
                 logger.info("[server]execut in thread pool...");
             }
-        });
+        }));
 
-        wrapExecutorService.submit(new Runnable() {
+        Executor executor = new LazyTraceExecutor(beanFactory, executorService);
+        executor.execute(new Runnable() {
             @Override
             public void run() {
-                logger.info("[server]wrap execut in thread pool...");
+                logger.info("[server]execut in lazyTraceExecutor...");
             }
         });
         return "hello";
--- spring-cloud-sleuth-stream-server/src/main/java/com/weasel/springcloud/sleuth/stream/server/controller/HelloController.java
+++ spring-cloud-sleuth-stream-server/src/main/java/com/weasel/springcloud/sleuth/stream/server/controller/HelloController.java
@@ -2,9 +2,19 @@ package com.weasel.springcloud.sleuth.stream.server.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.SpanNamer;
+import org.springframework.cloud.sleuth.TraceRunnable;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author dylan
@@ -14,10 +24,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private final static ExecutorService executorService = Executors.newCachedThreadPool();
+
+    @Autowired
+    private Tracer tracer;
+
+    @Autowired
+    private SpanNamer spanNamer;
+    @Autowired
+    private BeanFactory beanFactory;
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         logger.info("[server] reponse client hello...");
+        executorService.submit(new TraceRunnable(tracer,spanNamer,new Runnable(){
+            @Override
+            public void run() {
+                logger.info("[stream server]execut in thread pool...");
+            }
+        }));
+
+        Executor executor = new LazyTraceExecutor(beanFactory, executorService);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("[stream server]execut in lazyTraceExecutor...");
+            }
+        });
         return "hello";
     }
 }
